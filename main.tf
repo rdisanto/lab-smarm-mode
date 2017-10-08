f0e15096b097f081b5132bac5c84757bda4796a2
Romain DI SANTO <romain.disanto@gmail.com>
2017-10-09 00:07:50 +0200

    docker swarm mode provisionning

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # vps
 resource "openstack_compute_instance_v2" "vps" {
-    count = 2
+    count = 3
 
     name = "vps-swarm_mode-${(count.index)+1}"
     image_name = "Debian 9" # Image name
@@ -17,21 +17,35 @@ resource "openstack_compute_keypair_v2" "test_keypair" {
     provider = "openstack.ovh" # Provider's name in provider.tf
     name = "test_keypair" # Name of ssh key
     #https://github.com/rdisanto/public-keys/blob/patch-2/authorized_keys
-    public_key = "${file("~/.ssh/id_rsa.pub")}" # Path to ssh keys
+    public_key = "${file("~/.ssh/id_rsa.pub")}" # Path to ssh public keys
 }
 
 
 resource "null_resource" "ansible-provision"{
-    triggers {
-        cluster_instance_ids = "${join(",", openstack_compute_instance_v2.vps.*.id)}"
-    }
     count = 1
+
+    provisioner "local-exec" {
+        command = "echo \"[swarm-master]\" > swarm-inventory"
+    }
+
+    provisioner "local-exec" {
+        command = "echo \"${format("%s ansible_ssh_user=%s", openstack_compute_instance_v2.vps.0.access_ip_v4, var.ssh_user)}\" >> swarm-inventory"
+    }
+
+    provisioner "local-exec" {
+        command = "echo \"[swarm-nodes]\" >> swarm-inventory"
+    }
+
+    provisioner "local-exec" {
+        command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", openstack_compute_instance_v2.vps.*.access_ip_v4, var.ssh_user))}\" >> swarm-inventory"
+    }
+
     provisioner "local-exec" {
         connection {
             type = "ssh"
-            user = "debian"
+            user = "${(var.ssh_user)}"
             private_key = "${file("~/.ssh/id_rsa")}"
         }
-        command = "ansible-playbook --private-key=~/.ssh/id_rsa -u debian -i ${join(",", openstack_compute_instance_v2.vps.*.access_ip_v4)}, ansible/provision.yml"
+        command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --private-key=~/.ssh/id_rsa -u ${(var.ssh_user)} -i swarm-inventory ansible/provision.yml"
     }
 }
--- securityGroup.tf
+++ securityGroup.tf
@@ -9,4 +9,28 @@ resource "openstack_compute_secgroup_v2" "lab_swarm_mode" {
         ip_protocol = "tcp"
         cidr = "0.0.0.0/0"
     }
+    rule {
+        from_port = 2377
+        to_port = 2377
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
+    rule {
+        from_port = 7946
+        to_port = 7946
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
+    rule {
+        from_port = 7946
+        to_port = 7946
+        ip_protocol = "udp"
+        cidr = "0.0.0.0/0"
+    }
+    rule {
+        from_port = 4789
+        to_port = 4789
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
 }
